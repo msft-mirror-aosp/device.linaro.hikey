02abe92c47d0d3081aab0a7ea2232bc2e80f61f5
John Stultz <john.stultz@linaro.org>
2021-07-16 00:58:02 +0000

    hikey/hikey960: Drop old bluetooth HAL as its no longer needed
    With all modern kernels, we can use the upstream bluetooth
    interface and the standard bt linux HAL
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I5f1a954d3f0ace86e8a91b3a5acba079f835cd4a

--- device-common.mk
+++ device-common.mk
@@ -211,9 +211,6 @@ PRODUCT_COPY_FILES +=  \
         frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
         frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml
 
-# Include BT modules
-$(call inherit-product, device/linaro/hikey/wpan/ti-wpan-products.mk)
-
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
         frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
